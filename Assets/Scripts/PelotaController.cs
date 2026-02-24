aac209bac6e38f25a1e4dceba17cac43fcd774dc
25xabierif <a25xabierif@iessanclemente.net>
2026-02-24 13:45:49 +0100

    cambios martes afterEV

--- Assets/Scripts/PelotaController.cs
+++ Assets/Scripts/PelotaController.cs
@@ -1,58 +1,59 @@
-using UnityEngine;
 using System.Collections;
+using UnityEngine;
 
 public class PelotaController : MonoBehaviour
 {
 
-    Rigidbody2D rb;
+    private Rigidbody2D rb; 
     [SerializeField] float force;
     [SerializeField] float delay;
-    const float MIN_ANG = 25.0f;
-    const float MAX_ANG = 40.0f;
-    const float MAX_Y = 2.5f;
-    const float MIN_Y = -2.5f;
+
+    const float MIN_ANG = 25.0f; 
+    const float MAX_ANG = 40.0f; 
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        int direccionX = Random.Range(0, 2) == 0 ? -1 : 1;
-        StartCoroutine(LanzarPelota(direccionX));
+        //throwBall();
+        //Invoke("throwBall", delay);
+        transform.position = new Vector3(0,0,0); //Vector3.zero;
+         
+        int directionX = Random.Range(0, 2) == 0 ? -1 : 1; // El límite superior es exclusivo (el 2 quedaría fuera).
+        StartCoroutine(throwBall(directionX));
     }
 
+    IEnumerator throwBall(int directionX){
+
+        transform.position = new Vector3(0,0,0); //Vector3.zero;
+        rb.linearVelocity = Vector2.zero;
+
+        yield return new WaitForSeconds(delay);    
+        
+        float angulo = Random.Range(MIN_ANG, MAX_ANG) * Mathf.Deg2Rad;
+        int directionY = Random.Range(0,2) == 0 ?-1:1;
+
+        float x = Mathf.Cos(angulo) * directionX;
+        float y = Mathf.Sin(angulo) * directionY;
+         
+        rb.AddForce(new Vector2(x,y) * force, ForceMode2D.Impulse);   
+
+    }
+
+    // Update is called once per frame
     void Update()
     {
         
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        string tag = collision.gameObject.tag;
-        
-        if (tag.Equals("Pa1"))
-            Debug.Log("Colisión con Pala 1!");
-        else if (tag.Equals("Pa2"))
-            Debug.Log("Colisión con Pala 2!");
-    }
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        Debug.Log("Gol en " + other.tag + "!!");
-    }
-    IEnumerator LanzarPelota(int direccionX)
-    {
-        yield return new WaitForSeconds(delay);
+   private void OnTriggerEnter2D(Collider2D collider){
 
-        float posY = Random.Range(MIN_Y,MAX_Y);
-        transform.position = new Vector3(0,posY,0);
-        // Definimos el ángulo en radianes usando Range, especificando el mínimo y máximo.
-        float angulo = Random.Range(MIN_ANG, MAX_ANG) * Mathf.Deg2Rad;
-        float x = Mathf.Cos(angulo) * direccionX;
+        Debug.Log("Gol en " +collider.tag + "!!");
+        if(collider.tag.Equals("GoalLeft")){
+            StartCoroutine(throwBall(1));
+        }else if(collider.tag.Equals("GoalRigth")){
+            StartCoroutine(throwBall(-1));
+        }
 
-        // Determinamos si nos movemos hacia la derecha o izquierda.
-        // Si el valor devuelto es 0, la dirección en Y será negativa; si es 1, será positiva.
-        int direccionY = Random.Range(0, 2) == 0 ? -1 : 1;
-        float y = Mathf.Sin(angulo) * direccionY;
-
-        Vector2 impulso = new Vector2(x, y);
-        rb.AddForce(impulso * force, ForceMode2D.Impulse);
-    }
-// ...
+   }
 }
