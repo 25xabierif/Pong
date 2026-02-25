62e07befc8b232ddfb08187ba600537211d0433f
25xabierif <a25xabierif@iessanclemente.net>
2026-02-25 13:28:06 +0100

    rede posta

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,11 +6,23 @@ public class GameManager : MonoBehaviour
     int p1Score;
     int p2Score;
 
+    bool running = false;
+
     [SerializeField] TMP_Text txtP1Score;
     [SerializeField] TMP_Text txtP2Score;
 
+    [SerializeField] GameObject pelota;
+
     void Update() {
         Debug.Log("p1 = " + p1Score + "; p2 = " + p2Score);
+
+        if(!running && Input.GetKeyDown(KeyCode.Space)){
+            // Activamos la pelota 
+            pelota.SetActive(true);
+            // Indicamos que el juego ha comenzado
+            running = true; 
+        }
+
     }
 
     public void AddPointP1() 
